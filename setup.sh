71c723b33eb3090c5854dfa08d66517cd87cfd00
Koichi Nakamura <koichi@idein.jp>
2016-02-18 23:54:03 +0900

    Refine setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,12 @@
 DOT_FILES=(.gitconfig .tmux.conf)
 
 for file in ${DOT_FILES[@]}; do
-	ln -s $HOME/.dotfiles/$file $HOME/$file
+	ans=y
+	if [ -e $file ]; then
+		echo -n "Replace ${file}? [y/n]: "
+		read ans
+	fi
+	if [ "$ans" == "y" ]; then
+		ln -fs $HOME/.dotfiles/$file $HOME/$file
+	fi
 done
