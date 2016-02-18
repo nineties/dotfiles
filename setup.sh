ba8f9152159345baa8e96806e4b139d430fc6afa
Koichi Nakamura <koichi@idein.jp>
2016-02-19 02:40:09 +0900

    Remove meaningless tests from setup.sh

--- setup.sh
+++ setup.sh
@@ -3,18 +3,5 @@
 DOT_FILES=(.profile .gitconfig .tmux.conf .zshrc .vimrc)
 
 for file in ${DOT_FILES[@]}; do
-	replace=y
-
-    diff $HOME/.dotfiles/$file $HOME/$file > /dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        continue
-    fi
-
-	if [ -e $HOME/$file ]; then
-		echo -n "Replace ${file}? [y/n]: "
-		read replace
-	fi
-	if [ "$replace" == "y" ]; then
-		ln -fs $HOME/.dotfiles/$file $HOME/$file
-	fi
+    ln -fs $HOME/.dotfiles/$file $HOME/$file
 done
