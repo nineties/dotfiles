979c11f8f3b74bf2156b988a2134425704f1bf5d
Koichi Nakamura <koichi@idein.jp>
2017-08-14 21:50:29 +0900

    Add .screenrc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.profile .gitconfig .tmux.conf .zshrc .vimrc)
+DOT_FILES=(.profile .gitconfig .screenrc .zshrc .vimrc)
 
 for file in ${DOT_FILES[@]}; do
     ln -fs $HOME/.dotfiles/$file $HOME/$file
