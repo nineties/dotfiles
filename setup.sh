2639e40c933ed9fc72f6edb7574b438ca0e35b85
Koichi Nakamura <koichi@idein.jp>
2025-02-28 21:14:14 +0900

    Add .tmux.conf

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.profile .gitconfig .screenrc .zshrc .vimrc)
+DOT_FILES=(.profile .gitconfig .screenrc .zshrc .vimrc .tmux.conf)
 
 for file in ${DOT_FILES[@]}; do
     ln -fs $HOME/.dotfiles/$file $HOME/$file
